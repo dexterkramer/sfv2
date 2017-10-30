fbb69393e6ef5146025455f3bf8545577ee50fb7
dexterkramer <bsauveton@gmail.com>
2017-10-30 01:34:48 +0100

    wip

--- tools/dataManager.js
+++ tools/dataManager.js
@@ -29,9 +29,9 @@ var player = {
 	"links" : 
 	[
 		{
-			"field" : "fleats",
+			"field" : "fleat",
 			"object" : "fleat",
-			"relation" : "array"
+			"relation" : "single"
 		}
 	]
 };
@@ -49,56 +49,162 @@ var ship = {
 	]
 }
 
-var dataManager = function(databaseConnector){
+var dataManager = function(databaseTranslator){
 	this.datas = {};
+	this.objectByType = [];
+	this.openMode = false;
 	this.schema = [player, fleat, ship];
-    databaseConnector.setObjectSchema(this.schema);
-	this.databaseConnector = databaseConnector;
+	this.normalizeSchema();
+	if(typeof databaseTranslator != 'undefined')
+	{	
+		this.databaseTranslator = databaseTranslator;	
+		databaseTranslator.setObjectSchema(this.schema);
+	}
+    
 };
 
 dataManager.prototype = {
+	setSchema : function(schema)
+	{
+		this.schema = schema;
+	},
+	emptyDatas : function()
+	{
+		this.datas = null;
+		this.datas = {};
+		this.objectByType = [];
+	},
+	normalizeSchema : function()
+	{
+		var that = this;
+		this.schema.forEach(function(param, ind){
+			param.links.forEach(function(link, ind2){
+				var theIndex = that.schema.findIndex(function(elem){
+					return elem.object == link.object;
+				});
+				if(theIndex != -1)
+				{
+					that.schema[ind].links[ind2].object = that.schema[theIndex];
+				}
+			});
+		});
+	},
+	save : function(object)
+	{
+		if(typeof this.databaseTranslator != 'undefined')
+		{
+			this.databaseTranslator.save(this.extract(object));
+		}
+	},
+	getSchema : function(newDatas)
+	{
+		if(typeof newDatas == 'undefined' || typeof newDatas.schema == 'undefined' || !this.openMode)
+		{
+			return this.schema;
+		}
+		if(typeof newDatas != 'undefined' && typeof newDatas.schema != 'undefined')
+		{
+			return newDatas.schema;
+		}
+		return null;
+	},
     add : function(newDatas, refresh)
     {
         var newIndexes = this.addIndexes(newDatas, refresh);
         var that = this;
-        newDatas.schema.forEach(function(param, ind){
+        this.getSchema(newDatas).forEach(function(param, ind){
 			if(typeof newIndexes[param.object] != 'undefined')
 			{
 				newIndexes[param.object].forEach(function(id){
 					if(id != null)
 					{
-						that.normalizeItem(this.datas[param.object][id], param);
+						that.normalizeItem(that.datas[param.object][id], param);
 					}
 				})
 			}
         });
     }, 
+	extract : function(object, returnDatabase, param)
+	{
+		if(typeof returnDatabase == 'undefined')
+		{
+			var returnDatabase = {};
+		}
+		if(typeof param == 'undefined')
+		{
+			var param = this.getParamFromObject(object);
+		}
+		
+		if(objType != null)
+		{
+			if(typeof returnDatabase[param.object][object[param.idField]] == 'undefined' || returnDatabase[param.object][object[param.idField]] == null)
+			{
+				returnDatabase[param.object][object[param.idField]] = object;
+				param.links.forEach(function(link, ind){
+					if(link.relation == 'single' )
+					{
+						this.extract(object[link.object.object], returnDatabase, link.object);
+					}
+					else if(link.relation == 'array' )
+					{
+						object[link.object.object].forEach(function(obj, ind){
+							this.extract(obj, returnDatabase, link.object);
+						});
+					}
+				});
+			}
+			else
+			{
+				return returnDatabase;
+			}
+		}
+	},
+	getParamFromObject : function(object)
+	{
+		var param = null;
+		var index = this.objectByType.findIndex(function(elem){
+			return elem.object == object;
+		});
+		if(index != -1)
+		{
+			param = this.objectByType[index].param;
+		}
+		return param;
+	},
 	addIndexes : function(newDatas, refresh)
 	{
 		var newIndexes = {};
-		newDatas.schema.forEach(function(param, ind){
-			let index = this.schema.findIndex(function(elem){
-				return elem.object == param;
-			});
-			if(index == -1)
+		var that = this;
+		this.getSchema(newDatas).forEach(function(param, ind){
+			if(that.openMode)
 			{
-				this.schema.push(param);
+				let index = that.schema.findIndex(function(elem){
+					return elem.object == param.object;
+				});
+				if(index == -1)
+				{
+					that.schema.push(param);
+				}
 			}
 			if(typeof newDatas.datas[param.object] != 'undefined' && newDatas.datas[param.object] != null)
 			{
 				newDatas.datas[param.object].forEach(function(elem, ind){
-					if(typeof this.datas[param.object] != 'undefined' && this.datas[param.object] != null)
+					if(typeof that.datas[param.object] == 'undefined' || that.datas[param.object] == null)
 					{
-						this.datas[param.object] = [];
+						that.datas[param.object] = [];
 					}
-					if(typeof this.datas[param.object][elem[param.idField]] == 'undefined' || refresh)
+					if(typeof that.datas[param.object][elem[param.idField]] == 'undefined' || refresh)
 					{
 						if(typeof newIndexes[param.object] == 'undefined')
 						{
 							newIndexes[param.object] = [];
 						}
-						this.datas[param.object][elem[param.idField]] = JSON.parse(JSON.stringify(elem));
+						that.datas[param.object][elem[param.idField]] = JSON.parse(JSON.stringify(elem));
 						newIndexes[param.object].push(elem[param.idField]);
+						if(typeof that.datas[param.object][elem[param.idField]] == 'undefined')
+						{
+							that.objectByType.push({param : param, object : that.datas[param.object][elem[param.idField]] });
+						}
 					}
 				});
 			}
@@ -107,12 +213,13 @@ dataManager.prototype = {
 	},
     normalizeItem : function(elem, param)
     {	
+		var that = this;
         param.links.forEach(function(link, ind){
             if(link.relation == "one")
             {
                 if(elem[link.field] != null)
                 {
-                    elem[link.field] = this.datas[link.object][elem[link.field]];
+                    elem[link.field] = that.datas[link.object.object][elem[link.field]];
                 }
             }
             else if(link.relation == "array")
@@ -120,7 +227,7 @@ dataManager.prototype = {
                 elem[link.field].forEach(function(item, index){
                     if(item != null)
                     {
-                        elem[link.field][index] = this.datas[link.object][item];
+                        elem[link.field][index] = that.datas[link.object.object][item];
                     }
                 });
             }
---
+++ tools/mongoTranslator.js
@@ -0,0 +1,84 @@
+
+var playerDocument = {
+	'object' : 'player',
+	'document' : 'player'
+}	
+
+var fleatDocument = {
+	'object' : 'fleat',
+	'document' : 'fleat'
+}
+
+var mongoTranslator = function(translatorSchema){
+	this.translatorSchema = [playerDocument, fleatDocument];
+	this.mixedSchema = [];
+	this.tmpDataManager = new dataManager();
+};
+
+
+mongoTranslator.prototype = {
+	setObjectSchema : function(schema)
+	{
+		this.schema = schema;
+		this.tmpDataManager.setSchema(schema);
+		this.normalizeTranslatorSchema();
+	},
+	findInObjectSchema : function(paramName)
+	{
+		if(typeof this.schema != 'undefined' && this.schema != null)
+		{
+			var index = this.schema.findIndex(function(elem){
+				return elem.object == paramName;
+			});
+			if(index != -1)
+			{
+				return this.schema[index];
+			}
+		}
+		return null;
+	},
+	recurseLinks : function(schemaParam, objectList)
+	{
+        var that = this;
+		schemaParam.links.forEach(function(link, index){
+			var ind = objectList.findIndex(function(elem){
+				return elem == link.object.object;
+			});
+			if(ind == -1)
+			{
+				objectList.push(link.object.object);
+				that.recurseLinks(link.object, objectList);
+			}
+			return objectList;
+		});
+	},
+	normalizeTranslatorSchema : function()
+	{
+        var that = this;
+		this.translatorSchema.forEach(function(doc, ind){
+			var objectList = [];
+			var schemaParam = that.findInObjectSchema(doc.object);
+			if(schemaParam != null)
+			{
+				objectList.push(schemaParam.object);
+				that.recurseLinks(schemaParam, objectList);
+				that.mixedSchema[schemaParam.object] = objectList;
+			}
+		});
+	},
+	save : function(extract)
+	{
+		this.tmpDataManager.emptyDatas();
+		this.tmpDataManager.add(extract);
+		this.schema.forEach(function(param, ind){
+			if(typeof extract[param.object] != 'undefined' && extract[param.object] != null && typeof this.mixedSchema[param.object] != 'undefined' && this.mixedSchema[param.object] != null)
+			{
+				 
+			}
+		});
+	},
+	retrieve : function(object, ids)
+	{
+		// get all document with this object
+	}
+};
